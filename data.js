75fb042000dcb3e65f566a878d3d5dd87f97ac14
sxgpyjg <sxgpyjg@qq.com>
2026-06-27 23:25:01 +0800

    👌 2026-06-27_23:25:01

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-window.NOTE_DATA =[
+window.NOTE_DATA = [
   {
     "id": 1782311326522,
     "title": "蹙眉失神",
@@ -69,4 +69,4 @@ window.NOTE_DATA =[
     "content": "<img loading=\"lazy\" src=\"https://origin.picgo.net/2026/06/21/IMG_20250501_160556f48ec693c6391be9.jpg\" alt=\"配图\"><img loading=\"lazy\" src=\"https://origin.picgo.net/2026/06/21/IMG_20250501_1600492845c50145553f26.jpg\" alt=\"配图\"><img loading=\"lazy\" src=\"https://origin.picgo.net/2026/06/21/IMG_20250501_1603597080a29022e0284d.jpg\" alt=\"配图\"><img loading=\"lazy\" src=\"https://origin.picgo.net/2026/06/21/IMG_20250501_1613139f81195a77a8ad60.jpg\" alt=\"配图\">",
     "date": "2026-06-21"
   }
-];
+]
